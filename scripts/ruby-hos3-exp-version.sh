c2cebf78c8296d846c12ccbf7871af7176ab42bf
gnadgnaoh <117189041+gnadgnaoh@users.noreply.github.com>
2025-10-12 13:27:52 +0700

    Initial commit

---
+++ scripts/ruby-hos3-exp-version.sh
@@ -0,0 +1,8 @@
+echo "Add experimental version string"
+echo "-EXPERIMENTAL-HyperMoon" > localversion-00-experimental
+echo "Add commit version string"
+echo "-$(git rev-parse --short=7 HEAD)" > localversion-01-experimental
+echo "Add run number string"
+echo "-$RUN_NUMBER" > localversion-02-experimental
+make O=out kernelrelease
+echo "Done!"
